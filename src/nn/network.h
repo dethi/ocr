4bccfcd14bcfefb0e096147cfb17cb7a575c3874
Thibault Deutsch <thibault.deutsch@gmail.com>
2014-10-14 21:11:35 +0200

    start network

--- src/nn/network.h
+++ src/nn/network.h
@@ -1,4 +1,12 @@
 #ifndef _NETWORK_H_
 #define _NETWORK_H_
 
+typedef struct net {
+    struct neuron input[];
+    struct neuron hidden[];
+    struct neuron output;
+
+    void (*free) (struct net*);
+} net;
+
 #endif
