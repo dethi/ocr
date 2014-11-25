74df869c9acba8dbc6026312acde31ef54fc019d
Ilan <ilan.dubois@gmail.com>
2014-11-25 21:39:46 +0100

    Premieres fonction adaptee

--- src/proc/detection.h
+++ src/proc/detection.h
@@ -5,6 +5,14 @@
 #include <stdio.h>
 #include "../stb_image/stb_image_ext.h"
 
+struct lineData {
+    size_t X;
+    size_t Y;
+    size_t length;
+    size_t height;
+}
+
 void RLSA(t_img_desc *img, int i, int j);
+size_t charX(t_img_desc *img, struct lineData *ld);
 
 #endif
