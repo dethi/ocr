601b0d6fe1ad5567a48bfa3b2b074a4adf1affa8
BoBibelo <arthur.douillard@epita.fr>
2014-12-01 21:29:32 +0100

    Can now choose path of saved txt, but can't save twice...

--- not_build/gui/main.c
+++ not_build/gui/main.c
@@ -8,23 +8,26 @@ typedef struct
 
 gchar *img_name;
 gchar *txt_ocr = "test";
+gchar *txt_saved_name;
+gchar *txt_saved_path;
 
 GtkTextView *text_view = NULL;
 GtkTextBuffer *buffer = NULL;
+GtkWidget *dialog_save = NULL;
 
 void callback_about (GtkMenuItem *menuitem, gpointer user_data);
 void get_img (GtkFileChooserButton *wigdet, gpointer user_data);
 void ocr_text (GtkButton *widget, gpointer user_data);
 void save_text( GtkButton *widget, gpointer user_data);
-
+void save_dial (GtkButton *widget, gpointer user_data);
 
 int main(int argc, char *argv [])
 {
-    GtkWidget *fenetre_principale = NULL;
+    GtkWidget *main_window = NULL;
     SGlobalData data;
     GError *error = NULL;
     gchar *filename = NULL;
-    
+
     gtk_init(&argc, &argv);
 
     data.builder = gtk_builder_new();
@@ -43,9 +46,9 @@ int main(int argc, char *argv [])
 
     gtk_builder_connect_signals (data.builder, &data);
 
-    fenetre_principale = GTK_WIDGET(gtk_builder_get_object (data.builder, "window1"));
+    main_window = GTK_WIDGET(gtk_builder_get_object (data.builder, "window1"));
 
-    gtk_widget_show_all (fenetre_principale);
+    gtk_widget_show_all (main_window);
 
     gtk_main();
 
@@ -93,18 +96,49 @@ void ocr_text (GtkButton *widget, gpointer user_data)
     gtk_text_buffer_set_text ( buffer, txt_ocr, strlen(txt_ocr) );
 }
 
+void save_dial (GtkButton *widget, gpointer user_data)
+{
+    SGlobalData *data = (SGlobalData*) user_data;
+
+    dialog_save = GTK_WIDGET(gtk_builder_get_object(data->builder, "filechooserdialog1"));
+    gtk_dialog_run(GTK_DIALOG(dialog_save));
+    
+    gtk_widget_destroy(dialog_save);
+}   
+
+
 void save_text (GtkButton *widget, gpointer user_data)
 {
+    SGlobalData *data = (SGlobalData*) user_data;
+    GtkFileChooser *chooser = NULL;
+    chooser = GTK_FILE_CHOOSER(dialog_save);
+    GtkEntry *entry = NULL;
+    entry = GTK_ENTRY(gtk_builder_get_object(data->builder, "entry1"));
+    GtkEntryBuffer *buffer_entry = NULL;
+
+    buffer_entry = gtk_entry_get_buffer(entry);
+    txt_saved_name = gtk_entry_buffer_get_text(buffer_entry);
+    txt_saved_path = gtk_file_chooser_get_current_folder(chooser);
+
+    g_print("txt name choosen: %s\ntxt path choosen: %s\n", txt_saved_name, txt_saved_path);
+    
+
     GtkTextIter iter_start;
     GtkTextIter iter_end;
-
+    
     gtk_text_buffer_get_start_iter ( buffer, &iter_start);
     gtk_text_buffer_get_end_iter ( buffer, &iter_end);
-    
+
     gchar *text2save = gtk_text_buffer_get_text( buffer, &iter_start, &iter_end, TRUE);
     
+    gchar *txt_saved = (gchar*) strcat( txt_saved_path, "/");
+    txt_saved = (gchar*) strcat( txt_saved, txt_saved_name);
+    txt_saved = (gchar*) strcat( txt_saved, ".txt");
+    
+    g_print("Final name: %s\n", txt_saved);
+
     FILE *file = NULL;
-    file = fopen("file.txt", "w");
+    file = fopen( txt_saved, "w");
 
     if ( file == NULL ) {
         g_print("Error in opening .txt file !\n");
@@ -113,9 +147,10 @@ void save_text (GtkButton *widget, gpointer user_data)
     g_print(".txt saving...\n");
 
     fputs( text2save, file);
-    
+
     fclose ( file );
 
+    
 }
 
 
