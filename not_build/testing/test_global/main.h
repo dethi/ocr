a4f0af1ccf22b486781eca9e1fb90d4354ad44c2
Thibault Deutsch <thibault.deutsch@gmail.com>
2014-12-07 15:20:53 +0100

    add some test snippet

---
+++ not_build/testing/test_global/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "stb_image/stb_image_ext.h"
+#include "proc/preprocessing.h"
+#include "proc/detection.h"
+#include "proc/rotation.h"
+#include "proc/filter.h"
+
+int main(int argc, char *argv[]);
+
+#endif
