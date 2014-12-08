a9e5da89e1bee742dad55b7eb3ab53f9f1ceaa9a
Ilan <ilan.dubois@gmail.com>
2014-12-08 07:02:25 +0100

    Fonctionne sur ee.png

--- src/proc/xy-cut.c
+++ src/proc/xy-cut.c
@@ -83,6 +83,7 @@ void listAdd(struct coorList *l, uchar *data, size_t X, size_t Y)
 {
     printf("[INFO] listAdd()\n");
     struct coorList *aux = malloc(sizeof(struct coorList));
+    aux->next = l->next;
     l->next = aux;
     l = l->next;
     l->data = data;
